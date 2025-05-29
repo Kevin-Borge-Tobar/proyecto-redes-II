d061a33ded72f0ac2a5aa50e9ba27670cc856cce
Sincal200 <[sincaljulio200@gmail.com]>
2025-05-28 20:34:21 -0600

    Feat-Configuración De Provider Prinicipal

--- main/terraform-empresarios-sa/provider.tf
+++ main/terraform-empresarios-sa/provider.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Provider para sede central (probablemente Colombia)
 provider "aws" {
   region = "us-east-1"
+}
+
+# Providers para diferentes regiones
+provider "aws" {
+  alias  = "usa"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "espana"
+  region = "eu-west-1"
+}
+
+# Incluir todos los módulos de sedes
+module "sede_central" {
+  source = "./sedes"
+  # Variables necesarias
 }
